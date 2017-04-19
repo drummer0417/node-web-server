76a1e0eb4c8b903e62f62a03f47ff90beca6870f
drummer0417 <hans@androidappfactory.nl>
2017-04-19 09:16:56 +0200

    Added env var for testing only

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 var app = express();
 const port = process.env.PORT || 3000;
+const testVar = process.env.TESTVAR || "Var not found";
 
 var pageContent = 'This is quite some peace of useless text on this page... But it works, which is GREAT!';
 var currentYear = new Date().getFullYear();
@@ -13,15 +14,15 @@ app.set('view engine', 'hbs');
 
 // log incomming request
 app.use((req, res, next) => {
-    var msg = `${new Date().toString()}: ${req.method} ${req.url}`;
+  var msg = `${new Date().toString()}: ${req.method} ${req.url}`;
 
-    console.log(msg);
-    fs.appendFile('./log/server.log', `${msg}\n`, (error) => {
-        if (error) {
-            console.log(error);
-        }
-    });
-    next();
+  console.log(msg);
+  fs.appendFile('./log/server.log', `${msg}\n`, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+  next();
 });
 
 // to desplay maintenance page and not continue uncomment below method
@@ -40,38 +41,38 @@ hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
 app.get('/', (req, res) => {
-    res.render('welcome.hbs', {
-        siteName: 'Hans\' site',
-        pageTitle: 'Home sweet home',
-        welcomeText: 'Welcome to my brand new website.... Tot ziens!',
-        pageContent: pageContent
-    })
+  res.render('welcome.hbs', {
+    siteName: 'Hans\' site',
+    pageTitle: 'Home sweet home',
+    welcomeText: 'Welcome to my brand new website.... Tot ziens!',
+    pageContent: pageContent
+  })
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects.hbs', {
-        pageTitle: 'GitHub projects',
-        pageHeader: 'Projects:',
-        pageContent: 'Here you will find an overview of my GitHub Projects'
-    });
+  res.render('projects.hbs', {
+    pageTitle: 'GitHub projects',
+    pageHeader: 'Projects:',
+    pageContent: 'Here you will find an overview of my GitHub Projects'
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        pageTitle: 'Over ons',
-        pageContent: pageContent
-    });
+  res.render('about.hbs', {
+    pageTitle: 'Over ons',
+    pageContent: pageContent
+  });
 });
 
 app.get('/bad', (req, res) => {
-    res.send({
-        error: 'Bad request. Check your url, correct it and try again... mafkees',
-        nickname: ['slome',
-            'mafkees', 'gek'
-        ]
-    })
+  res.send({
+    error: 'Bad request. Check your url, correct it and try again... mafkees',
+    nickname: ['slome',
+      'mafkees', 'gek'
+    ]
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Server is up & running on port: ${port} `);
+  console.log(`Server is up & running on port: ${port} `);
 })
